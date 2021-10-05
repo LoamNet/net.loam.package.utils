e55b6cf9193302a41b4f6ab2420a5063f2c76118
Matthew Cech <contact@matthewcech.com>
2021-10-04 18:13:34 -0700

    1.0.4 Added Copy Meta GUID to Clipboard asset option.

---
+++ Scripts/Editor/AssetMenuItems.cs
@@ -0,0 +1,44 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+namespace Loam.Editor
+{
+    /// <summary>
+    /// Menu items that appear in right-click in 'Project' view and in Unity's Assets menu
+    /// </summary>
+    public static class AssetMenuItems
+    {
+        /// <summary>
+        /// Attempt to find the GUID of the active object. If multiple are selected,
+        /// then the first one selected is considered active. If something in-scene is
+        /// selected, it will show up as active but won't have a valid path.
+        /// </summary>
+        [MenuItem("Assets/Loam/Copy .meta GUID to clipboard", priority = 10)]
+        public static void MetaGUIDToClipboard()
+        {
+            Object active = Selection.activeObject;
+
+            if (active != null)
+            {
+                string path = AssetDatabase.GetAssetPath(active);
+                string guid = AssetDatabase.AssetPathToGUID(path);
+
+                if (!string.IsNullOrEmpty(path) && !string.IsNullOrEmpty(guid))
+                {
+                    GUIUtility.systemCopyBuffer = guid;
+                    Debug.Log($"Copied GUID '{guid}' to clipboard from '{path}'");
+                }
+                else
+                {
+                    Debug.LogWarning($"Unable to copy GUID to clipboard for object '{active.name}' - Object likely not located in the assets directory.");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Unable to copy GUID to clipboard - No item selected!");
+            }
+        }
+    }
+}
+#endif
